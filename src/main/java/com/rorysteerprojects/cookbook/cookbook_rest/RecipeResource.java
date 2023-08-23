5e4c394a3ed3a28e522e69af7f84c05d61deb04b
rory <rsteer34@gmail.com>
2023-08-23 17:01:52 -0600

    Updated Recipe resource to include date in hardcoded return data.

--- src/main/java/com/rorysteerprojects/cookbook/cookbook_rest/RecipeResource.java
+++ src/main/java/com/rorysteerprojects/cookbook/cookbook_rest/RecipeResource.java
@@ -4,12 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 public class RecipeResource {
 
-    private static List<Recipe> RECIPES_LIST = List.of(new Recipe(1, "R1"), new Recipe(2, "R2"));
+    private static List<Recipe> RECIPES_LIST = List.of(
+            new Recipe(1, "R1", LocalDate.of(2023, 2, 3)),
+            new Recipe(2, "R2", LocalDate.of(2023, 3, 3))
+    );
     private Logger logger = LoggerFactory.getLogger(getClass());
     @GetMapping("/recipes")
     public List<Recipe> getRecipes() {
@@ -29,4 +33,4 @@ public class RecipeResource {
     }
 }
 //SecurityProperties.BASIC_AUTH_ORDER
-record Recipe (int id, String name) {}
+record Recipe (int id, String name, LocalDate lastCooked) {}
